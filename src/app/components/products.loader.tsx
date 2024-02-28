15bcd325740f273e9a3464f70494464cda77305f
Patrick Rivera <patrickrivera@Patricks-MacBook-Pro.local>
2024-02-28 23:19:19 +0800

    master

--- src/app/components/products.loader.tsx
+++ src/app/components/products.loader.tsx
@@ -6,37 +6,41 @@ import ProductThumbnail from './product.thumbnail';
 
 const ProductsLoader: React.FC<{ page: number, limit: number, target: string }> = ({ page, limit, target }) => {
   const [isLoading, setLoading] = useState(false)
+  const [hasMoreItems, setMoreItems] = useState(true)
   const [currentPage, setPage] = useState(page)
-  const [productList, setProducts] = useState([])
-  let container = null
+  const [productList, setProducts] = useState<Product[]>([])
+  const [container, setContainer] = useState<HTMLElement>()
 
   useEffect(() => {
-    let children = [];
-    productList.map(product=>{
-      children.push( <ProductThumbnail product={product}/>)
+    let children: React.ReactElement[] = [];
+    productList.map((product:Product)=>{
+      children.push( <ProductThumbnail key={product.id} product={product}/>)
     })
-    if(!container) {
-      const targetElement = document.getElementById(target);
-      container = createRoot(targetElement)
-    }
-    
-    container.render(children)
+
+    if(container) container.render(children)
 
     setLoading(false);
-  }, [productList, target])
+  }, [productList, target, container])
 
   function loadMore() {
+    let skip = currentPage * limit
     setPage(currentPage+1)
     setLoading(true)
-    
-    let skip = currentPage * limit
-  
-    return fetch('https://dummyjson.com/products?limit=10&skip='+skip)
+
+    return fetch(`https://dummyjson.com/products?limit=${limit}&skip=${skip}`)
       .then((res) => res.json())
       .then((data:Products) => {
         const ProductList = [...productList, ...data.products];
         setProducts(ProductList);
-
+        if(!container) {
+          const targetElement = document.getElementById(target);
+          if(targetElement){
+            const rootElement = createRoot(targetElement)
+            setContainer(rootElement);
+          }
+        }
+        
+        if(data.skip+data.limit >= data.total) setMoreItems(false)
     })
   }
 
@@ -44,9 +48,11 @@ const ProductsLoader: React.FC<{ page: number, limit: number, target: string }> 
     return <button>Loading...</button>
   }
 
-  return (
-    <button onClick={loadMore}>Load More</button>
-  )
+  if(hasMoreItems) {
+      return (
+          <button onClick={loadMore}>Load More</button>
+      )
+  }
 };
 
 export default ProductsLoader
